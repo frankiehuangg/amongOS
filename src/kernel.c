b5138608d6080aef30c1a2bdcf93ddd72fe1b7da
frankiehuangg <frankie.huangg@gmail.com>
2023-02-21 10:40:40 +0700

    [RMV] include .c in file

--- src/kernel.c
+++ src/kernel.c
@@ -4,14 +4,15 @@
 #include "lib-header/gdt.h"
 #include "lib-header/framebuffer.h"
 #include "lib-header/kernel_loader.h"
-#include "framebuffer.c"
 
 void kernel_setup(void) {
     framebuffer_clear();
+
     framebuffer_write(3, 8,  'H', 0, 0xF);
     framebuffer_write(3, 9,  'a', 0, 0xF);
     framebuffer_write(3, 10, 'i', 0, 0xF);
     framebuffer_write(3, 11, '!', 0, 0xF);
-    framebuffer_set_cursor(3, 10);
+    
+	framebuffer_set_cursor(3, 10);
     while (TRUE);
 }

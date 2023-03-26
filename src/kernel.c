918404069c48579ada5f6f575e68cb540f5f9272
irsyadnb <irsyadxd@gmail.com>
2023-03-26 17:03:14 +0700

    for interrupt testing

--- src/kernel.c
+++ src/kernel.c
@@ -5,14 +5,23 @@
 #include "lib-header/framebuffer.h"
 #include "lib-header/kernel_loader.h"
 
-void kernel_setup(void) {
-    // framebuffer_clear();
+// void kernel_setup(void) {
+//     // framebuffer_clear();
 
-    framebuffer_write(3, 8,  'H', 0, 0xF);
-    framebuffer_write(3, 9,  'a', 0, 0xF);
-    framebuffer_write(3, 10, 'i', 0, 0xF);
-    framebuffer_write(3, 11, '!', 0, 0xF);
+//     framebuffer_write(3, 8,  'H', 0, 0xF);
+//     framebuffer_write(3, 9,  'a', 0, 0xF);
+//     framebuffer_write(3, 10, 'i', 0, 0xF);
+//     framebuffer_write(3, 11, '!', 0, 0xF);
     
-	framebuffer_set_cursor(3, 10);
+// 	framebuffer_set_cursor(3, 10);
+//     while (TRUE);
+// }
+void kernel_setup(void) {
+    enter_protected_mode(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+    __asm__("int $0x4");
     while (TRUE);
 }

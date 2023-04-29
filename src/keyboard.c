7f814d0c1bc632a9f5f8ebee355435976ee5cd42
farizkik <fariski.mail@gmail.com>
2023-04-29 13:32:53 +0700

    fix: implementation

--- src/keyboard.c
+++ src/keyboard.c
@@ -31,9 +31,9 @@ static struct KeyboardDriverState keyboard_state = {FALSE,FALSE, 0 ,{0} };
 /* -- Driver Interfaces -- */
 
 // Activate keyboard ISR / start listen keyboard & save to buffer
-void keyboard_state_activate(void){
-	activate_keyboard_interrupt();
-    keyboard_state.keyboard_input_on = 1;
+void keyboard_state_activate(void) {
+	memset(keyboard_state.keyboard_buffer, 0, KEYBOARD_BUFFER_SIZE);
+	keyboard_state.keyboard_input_on = 1;
 }
 
 // Deactivate keyboard ISR / stop listening keyboard interrupt
@@ -43,9 +43,7 @@ void keyboard_state_deactivate(void){
 
 // Get keyboard buffer values - @param buf Pointer to char buffer, recommended size at least KEYBOARD_BUFFER_SIZE
 void get_keyboard_buffer(char *buf){
-    for (uint8_t i = 0; i < keyboard_state.buffer_index; i++) {
-        buf[i] = keyboard_state.keyboard_buffer[i];  
-    }  
+	memcpy(buf, keyboard_state.keyboard_buffer, KEYBOARD_BUFFER_SIZE);
 }
 
 // Check whether keyboard ISR is active or not - @return Equal with keyboard_input_on value
@@ -63,56 +61,65 @@ void keyboard_isr(void)
 		char 	mapped_char	= keyboard_scancode_1_to_ascii_map[scancode];
 
 		// TODO: Implement scancode processing
-		keyboard_state.buffer_index = 0;
 
+		keyboard_state.keyboard_buffer[keyboard_state.buffer_index] = mapped_char;
 
+		// Menulis ke buffer
 		uint16_t pos = get_cursor_position();
 
 		int row = pos / 80;
 		int col = pos % 80;
-		if (scancode == EXTENDED_SCANCODE_BYTE) {
-            keyboard_state.read_extended_mode = TRUE;
-        } else if (keyboard_state.read_extended_mode) {
-            keyboard_state.read_extended_mode = FALSE;
-            switch (scancode) {
-                case EXT_SCANCODE_UP:
-                    if (row>=1){
-                        framebuffer_set_cursor(row-1,col);
-                    }
-                    break;
-                case EXT_SCANCODE_DOWN:
-					if (row<24){
-                        framebuffer_set_cursor(row+1,col);
-					}
-                    break;
-                case EXT_SCANCODE_LEFT:
-                    if(col>=1){
-                        framebuffer_set_cursor(row,col-1);
-                    }
-                    break;
-                case EXT_SCANCODE_RIGHT:
-					if(col<=80){		
-    			        framebuffer_set_cursor(row,col+1);
-					}
-                    break;
-                default:
-                    break;
-			}
-		}
-		else if (mapped_char == '\n')
+		// if (scancode == EXTENDED_SCANCODE_BYTE) {
+        //     keyboard_state.read_extended_mode = TRUE;
+        // } else if (keyboard_state.read_extended_mode) {
+        //     keyboard_state.read_extended_mode = FALSE;
+        //     switch (scancode) {
+        //         case EXT_SCANCODE_UP:
+        //             if (row>=1){
+        //                 framebuffer_set_cursor(row-1,col);
+        //             }
+        //             break;
+        //         case EXT_SCANCODE_DOWN:
+		// 			if (row<24){
+        //                 framebuffer_set_cursor(row+1,col);
+		// 			}
+        //             break;
+        //         case EXT_SCANCODE_LEFT:
+        //             if(col>=keyboard_state.buffer_index){
+        //                 framebuffer_set_cursor(row,col-1);
+        //             }
+        //             break;
+        //         case EXT_SCANCODE_RIGHT:
+		// 			if(col<=80){		
+    	// 		        framebuffer_set_cursor(row,col+1);
+		// 			}
+        //             break;
+        //         default:
+        //             break;
+		// 	}
+		// }
+		if (mapped_char == '\n')
 		{
-			if (row <24){
-				framebuffer_set_cursor(row + 1, 0);
-				keyboard_state.buffer_index = 0;
+			if (row ==24){
+				framebuffer_shift();
 			}
+			else {
+				framebuffer_set_cursor(row + 1, 0);
+			}
+			
+			keyboard_state.buffer_index = 0;
+
+			keyboard_state_deactivate();
 		}
 
 		else if (mapped_char == '\b')
 		{
-			if (col != 0)
+			if (keyboard_state.buffer_index != 0)
 			{
 				framebuffer_write(row, col-1, 0, 0xF, 0);
 				framebuffer_set_cursor(row, col-1);
+
+				keyboard_state.keyboard_buffer[keyboard_state.buffer_index - 1] = '\0';
 
 				keyboard_state.buffer_index--;
 			}
@@ -133,7 +140,6 @@ void keyboard_isr(void)
 			framebuffer_write(row, col, mapped_char, 0xF, 0);
 			framebuffer_set_cursor(row, col+1);
 
-			keyboard_state.keyboard_buffer[keyboard_state.buffer_index] = mapped_char;
 			keyboard_state.buffer_index++;
 		}
 	}
